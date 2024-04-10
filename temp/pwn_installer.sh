5edcf66a586b9bc6b18703827c4599815139dc30
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-04-10 11:45:20 +0900

    Update pwn_installer.sh

--- temp/pwn_installer.sh
+++ temp/pwn_installer.sh
@@ -1,6 +1,6 @@
-sudo apt-get install git -y
+sudo apt-get install git -Y
 sudo apt-get update
-sudo apt-get install python3 python3-dev python3-pip 
+sudo apt-get install python3 python3-dev python3-pip -Y
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
