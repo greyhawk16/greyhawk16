cc7d1631bb3a30f98dbcd1c1b4b959076a6d92de
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-04-10 11:25:50 +0900

    Update and rename pwngdb_installer.sh to pwn_installer.sh
    - pwntool 설치 명령 수정
    - "pwngdb_installer.sh" 에서 "pwn_installer.sh"로 이름변경

---
+++ temp/pwn_installer.sh
@@ -0,0 +1,10 @@
+sudo apt-get install git -y
+sudo apt-get update
+sudo apt-get install python3 python3-dev python3-pip 
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
+
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
--- temp/pwngdb_installer.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install gdb
-git clone https://github.com/pwndbg/pwndbg
-cd pwndbg
-./setup.sh
