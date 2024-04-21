b58371c69ad8543d62e4c5343aa6569f3ff693a8
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-04-21 10:34:32 +0900

    ✨ git 설치명령 추가
    `git` 설치 명령 추가

--- temp/pwn_installer.sh
+++ temp/pwn_installer.sh
@@ -1,6 +1,6 @@
 sudo apt-get install git -Y
 sudo apt-get update
-sudo apt-get install python3 python3-dev python3-pip -Y
+sudo apt-get install python3 python3-dev python3-pip git -Y
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
